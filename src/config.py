8147e849553e3799c84b27fee9675d2fdfe11a89
Michele Marchetti <michele-marchetti@hotmail.it>
2025-04-20 13:09:04 +0200

    "Aggiunto il file di configurazione con sezione descrittiva per future impostazioni dell'applicazione."

--- src/config.py
+++ src/config.py
@@ -1,1 +1,17 @@
 
+
+"""
+Modulo di configurazione per l'applicazione InvoiceReader.
+
+Questo file è destinato a contenere costanti, variabili di configurazione
+e impostazioni che possono essere modificate per personalizzare il comportamento
+dell'applicazione senza dover modificare il codice principale.
+
+Esempi di possibili configurazioni future:
+- Formati di data supportati
+- Pattern di espressioni regolari per l'estrazione di informazioni
+- Formati di nome file personalizzati
+- Impostazioni di interfaccia utente
+"""
+
+# Configurazioni future verranno aggiunte qui
